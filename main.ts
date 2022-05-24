07910ed5a9f4c6926af98ff4405dfddb2cb1a765
Justin Lo <justinlctstudy96@gmail.com>
2022-05-24 15:45:09 +0800

    for eye branch

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
-// import express from 'express';
-import * as express from 'express';
+import express from 'express';
+// import * as express from 'express';
 import {Request,Response} from 'express';
 import * as path from "path";
 
